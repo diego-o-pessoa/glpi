b79e1eb713a55958725a969f515195ba76d9119a
Diego <diego.pessoa2712@gmail.com>
2026-09-18 12:36:57 -0300

    Update: Senha do Banco de Dados atualizada

--- glpi_data/config/config_db.php
+++ glpi_data/config/config_db.php
@@ -2,8 +2,7 @@
 class DB extends DBmysql {
    public $dbhost = 'mysql';
    public $dbuser = 'glpiuser';
-   //public $dbpassword = 'Ativ%40Adm2026';
-   public $dbpassword = 'Ativ@Adm2026';
+   public $dbpassword = 'Ativ%40Adm2026';
    public $dbdefault = 'glpidb';
    public $use_timezones = true;
    public $use_utf8mb4 = true;
